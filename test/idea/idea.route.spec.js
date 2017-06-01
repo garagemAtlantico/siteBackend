818c2e1e4fa0d9d4b08fde0e6f3da6b48616dbdc
Joao Pereira <joaopapereira@gmail.com>
2017-06-01 08:39:50 -0400

    Initial import

---
+++ test/idea/idea.route.spec.js
@@ -0,0 +1,14 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
+
+var Idea = require(process.cwd() + '/src/story/story.model');
+
+let expect = require('chai').expect
+
+
+describe('Name of the group', function() {
+  
+});
+  
